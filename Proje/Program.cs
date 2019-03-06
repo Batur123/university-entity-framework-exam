b6be220719ed96735e9b6de93779617181634182
Batuhan Özkoç <batur.ozkoc@gmail.com>
2019-03-06 23:03:16 +0300

    Starting Form
    Starting form is updated.

--- Proje/Program.cs
+++ Proje/Program.cs
@@ -16,7 +16,7 @@ namespace Proje
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new KayitOl());
+            Application.Run(new BaslangicVeritabaniSayfasi());
         }
     }
 }
